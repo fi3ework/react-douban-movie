638950ea54df62ab22bbbe2c6785dbf580c50888
Wee <fi3ework@gmail.com>
2018-08-17 00:18:44 +0800

    fix(proxy): fix proxy path for prod env

--- src/constants.js
+++ src/constants.js
@@ -1,30 +1,27 @@
-// 使用Nginx代理
-// const HOST = process.env.NODE_ENV === 'production' ? '/proxy' : 'http://localhost:8081'
-// 使用node代理
-const HOST = process.env.NODE_ENV === 'production' ? 'http://118.24.21.99:8081' : 'http://localhost:8081'
+const HOST =
+  process.env.NODE_ENV === "production" ? "/proxy" : "http://localhost:8081";
 
 // 正在热映
-export const API_IN_THEATERS = `${HOST}/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_IN_THEATERS = `${HOST}/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 即将上映
-export const API_COMING_SOON = `${HOST}/movie/coming_soon?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_COMING_SOON = `${HOST}/movie/coming_soon?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 周口碑榜
-export const API_WEEKLY = `${HOST}/movie/weekly?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_WEEKLY = `${HOST}/movie/weekly?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 新片榜
-export const API_NEW_MOVIES = `${HOST}/movie/new_movies?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_NEW_MOVIES = `${HOST}/movie/new_movies?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 北美票房榜
-export const API_US_BOX = `${HOST}/movie/us_box?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_US_BOX = `${HOST}/movie/us_box?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 北美票房榜
-export const API_TOP_250 = `${HOST}/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_TOP_250 = `${HOST}/movie/top250?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 电影条目
-export const API_MOVIE_SUBJECT = `${HOST}/movie/subject/:id?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_MOVIE_SUBJECT = `${HOST}/movie/subject/:id?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 长评
-export const API_MOVIE_REVIEWS = `${HOST}/movie/subject/:id/reviews?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_MOVIE_REVIEWS = `${HOST}/movie/subject/:id/reviews?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 短评
-export const API_MOVIE_COMMENTS = `${HOST}/movie/subject/:id/comments?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_MOVIE_COMMENTS = `${HOST}/movie/subject/:id/comments?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 影人页
-export const API_CELEBRITY = `${HOST}/movie/celebrity/:id?apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_CELEBRITY = `${HOST}/movie/celebrity/:id?apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // 搜索页
-export const API_SEARCH = `${HOST}/movie/search?q=:query&apikey=0b2bdeda43b5688921839c8ecb20399b`
+export const API_SEARCH = `${HOST}/movie/search?q=:query&apikey=0b2bdeda43b5688921839c8ecb20399b`;
 // Tag
-export const API_TAG = `${HOST}/movie/search?tag=:tag&apikey=0b2bdeda43b5688921839c8ecb20399b`
-
+export const API_TAG = `${HOST}/movie/search?tag=:tag&apikey=0b2bdeda43b5688921839c8ecb20399b`;
